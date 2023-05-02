d0db8b0805ea08d588d1b485267e98a627471544
VORONIN Roman <voronin.roman.i@gmail.com>
2023-05-02 18:03:36 +0200

    AOP; Scheduled; Endpoint; Batch

--- src/main/java/com/cecilireid/springchallenges/CateringJobsEndpoint.java
+++ src/main/java/com/cecilireid/springchallenges/CateringJobsEndpoint.java
@@ -1,12 +1,36 @@
 package com.cecilireid.springchallenges;
 
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
 @Component
+@Endpoint(id = "status")
+
 public class CateringJobsEndpoint {
     private final CateringJobRepository cateringJobRepository;
 
     public CateringJobsEndpoint(CateringJobRepository cateringJobRepository) {
         this.cateringJobRepository = cateringJobRepository;
+    }
+
+    @ReadOperation
+    public Map<Status, Long> getAllJobsForStatus() {
+       /* Map<Status, Integer> mapStatus = new HashMap<>();
+         Arrays.asList(Status.values()).stream()
+                .forEach(status -> {mapStatus.put(status,cateringJobRepository.findByStatus(status).size() ); });
+*/
+        return cateringJobRepository.findAll()
+                .stream()
+                .collect(groupingBy(CateringJob::getStatus, Collectors.counting()));
+
     }
 }
--- src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
+++ src/main/java/com/cecilireid/springchallenges/ScheduledTasks.java
@@ -2,13 +2,21 @@ package com.cecilireid.springchallenges;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private final CateringJobRepository cateringJobRepository;
 
+    public ScheduledTasks(CateringJobRepository cateringJobRepository) {
+        this.cateringJobRepository = cateringJobRepository;
+    }
+
+
+    @Scheduled(cron = "*/10 * * * * *")
     public void reportOrderStats() {
-        
+        logger.info("Number of jobs : "+cateringJobRepository.findAll().size());
     }
 }
